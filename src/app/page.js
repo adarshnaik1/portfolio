ec1bececfc53533379fc5f3eb9f41228fea69686
Adarsh Naik <143270320+adarshnaik1@users.noreply.github.com>
2025-11-09 22:01:26 +0530

    hero with profile icon

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,55 @@
+
 import Image from "next/image";
 
+import { PointerHighlight } from "@/components/ui/pointer-highlight";
+
 export default function Home() {
+ 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <>
+    <div className=" flex flex-col items-center justify-center h-screen">
+        {/*This gives the background the glowy effect*/}
+       <div className="absolute inset-0 -z-10">
+        <div className="absolute top-1/4 left-1/4 w-[500px] h-[500px] bg-gradient-to-r from-purple-500 to-blue-500 rounded-full blur-3xl opacity-30"></div>
+        <div className="absolute bottom-1/4 right-1/4 w-[400px] h-[400px] bg-gradient-to-r from-pink-500 to-yellow-500 rounded-full blur-3xl opacity-20"></div>
+      </div>
+
+    <div className="flex items-center justify-center h-full space-x-12 p-4">
+      {/*Profile Section */}
+      <div className="w-36 h-36 rounded-full overflow-hidden border-3 border-white hover:scale-110 transition-transform duration-300">
+             <Image
+        src="/profile.jpeg"
+        height={150}
+        width={150}
+        className="object-cover"
+        alt="picture of adarsh"
+      />
+      </div>
+
+      {/* Profile Hero Section */}
+
+      <div className="flex flex-col items-center space-y-2">
+       <h1 className="text-6xl font-[Consolas] tracking-wider"> Hey There! </h1>
+       <PointerHighlight >
+            <h1  className="text-6xl font-[Consolas] tracking-wider"> I'm Adarsh Naik</h1>
+       </PointerHighlight>
+       
+      </div>
+
+ 
+
     </div>
+   
+        <div className="flex   ">
+          <h1 className="text-xl  font-[Consolas] tracking-wider">More Content Coming Soon! </h1>
+        </div>
+    
+   
+      
+
+  </div>
+
+    </>
+    
   );
 }
